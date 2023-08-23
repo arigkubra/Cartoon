ead05c019fb50ff492adcc5690b07f269bae231c
arigkubra <arigkubra@gmail.com>
2023-08-23 10:08:06 +0300

    view has been edited

--- src/views/CartoonView.vue
+++ src/views/CartoonView.vue
@@ -1,21 +1,19 @@
 <template >
-    <div class="flex  flex-wrap pt-12 justify-center items-center bg-gradient-to-b from-black to-grey h-screen w-screen">
-        <Cartoon />
-        <Cartoon />
-        <Cartoon />
-        <Cartoon />
-        <Cartoon />
-        <Cartoon />
-    </div>
+  <div
+    class="flex flex-wrap pt-12 justify-center items-center bg-gradient-to-b from-black to-grey h-screen w-screen">
+    <Cartoon />
+    <Cartoon />
+    <Cartoon />
+    
+  </div>
 </template>
 
 <script>
-import Cartoon from '@/components/Cartoon.vue';
+import Cartoon from "@/components/Cartoon.vue";
 
-export default{
-    components:{
-        Cartoon
-    }
-}
-
+export default {
+  components: {
+    Cartoon,
+  },
+};
 </script>
