49037327cd2fcce196611940c8755d57071eb825
arigkubra <arigkubra@gmail.com>
2023-08-23 10:44:20 +0300

    Row component has been created

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,35 +1,33 @@
 <template>
   <div class="bg-gradient-to-b from-black to-grey">
-    <h1 class="text-6xl flex justify-center items-center py-10 text-white">About Us</h1>
+    <h1 class="text-6xl flex justify-center items-center py-11 text-white">About Us</h1>
 
-    <div class="flex flex-col justify-center items-center">
-      <div class="collapse w-2/6 my-5 collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" />
-        <div class="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
-        <div class="collapse-content">
-          <p>hello</p>
-        </div>
-      </div>
-      <div class="collapse w-2/6 my-5 collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" />
-        <div class="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
-        <div class="collapse-content">
-          <p>hello</p>
-        </div>
-      </div>
-      <div class="collapse w-2/6 my-5 collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" />
-        <div class="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
-        <div class="collapse-content">
-          <p>hello</p>
-        </div>
-      </div>
+    <div class="overflow-x-auto mx-16 pt-6 pb-24">
+      <table class="table">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <Row />
+          <Row />
+          <Row />
+          <Row />
+        </tbody>
+        
+      </table>
     </div>
   </div>
 </template>
+<script>
+import Row from '@/components/Row.vue';
+export default{
+  components:{
+    Row,
+  }
+}
+</script>
